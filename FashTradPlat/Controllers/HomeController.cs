1b027112c5ce734a348460cbfc1917885fe73700
ac116539 <ac116539@8CG8163KXX>
2024-04-04 14:27:00 +1300

    Added Contact Us page

--- FashTradPlat/Controllers/HomeController.cs
+++ FashTradPlat/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace FashTradPlat.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
