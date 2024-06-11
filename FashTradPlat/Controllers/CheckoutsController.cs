6119df43c00da040a4eabaff48c3532cad3ae81c
Rahul Shetty <ac116539@avcol.school.nz>
2024-06-11 14:59:00 +1200

    Controllers

---
+++ FashTradPlat/Controllers/CheckoutsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FashTradPlat.Areas.Identity.Data;
+using FashTradPlat.Models;
+
+namespace FashTradPlat.Controllers
+{
+    public class CheckoutsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CheckoutsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Checkouts
+        public async Task<IActionResult> Index()
+        {
+              return _context.Checkouts != null ? 
+                          View(await _context.Checkouts.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Checkouts'  is null.");
+        }
+
+        // GET: Checkouts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Checkouts == null)
+            {
+                return NotFound();
+            }
+
+            var checkout = await _context.Checkouts
+                .FirstOrDefaultAsync(m => m.Checkout_ID == id);
+            if (checkout == null)
+            {
+                return NotFound();
+            }
+
+            return View(checkout);
+        }
+
+        // GET: Checkouts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Checkouts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Checkout_ID,Price,Transaction_ID")] Checkout checkout)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(checkout);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(checkout);
+        }
+
+        // GET: Checkouts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Checkouts == null)
+            {
+                return NotFound();
+            }
+
+            var checkout = await _context.Checkouts.FindAsync(id);
+            if (checkout == null)
+            {
+                return NotFound();
+            }
+            return View(checkout);
+        }
+
+        // POST: Checkouts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Checkout_ID,Price,Transaction_ID")] Checkout checkout)
+        {
+            if (id != checkout.Checkout_ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(checkout);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CheckoutExists(checkout.Checkout_ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(checkout);
+        }
+
+        // GET: Checkouts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Checkouts == null)
+            {
+                return NotFound();
+            }
+
+            var checkout = await _context.Checkouts
+                .FirstOrDefaultAsync(m => m.Checkout_ID == id);
+            if (checkout == null)
+            {
+                return NotFound();
+            }
+
+            return View(checkout);
+        }
+
+        // POST: Checkouts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Checkouts == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Checkouts'  is null.");
+            }
+            var checkout = await _context.Checkouts.FindAsync(id);
+            if (checkout != null)
+            {
+                _context.Checkouts.Remove(checkout);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CheckoutExists(int id)
+        {
+          return (_context.Checkouts?.Any(e => e.Checkout_ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ FashTradPlat/Controllers/TransactionsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FashTradPlat.Areas.Identity.Data;
+using FashTradPlat.Models;
+
+namespace FashTradPlat.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transactions
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Transactions.Include(t => t.Checkout).Include(t => t.Payment);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Transactions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Transactions == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions
+                .Include(t => t.Checkout)
+                .Include(t => t.Payment)
+                .FirstOrDefaultAsync(m => m.Transaction_ID == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(transaction);
+        }
+
+        // GET: Transactions/Create
+        public IActionResult Create()
+        {
+            ViewData["Checkout_ID"] = new SelectList(_context.Checkouts, "Checkout_ID", "Checkout_ID");
+            ViewData["Payment_ID"] = new SelectList(_context.Payments, "Payment_ID", "Payment_ID");
+            return View();
+        }
+
+        // POST: Transactions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Transaction_ID,Payment_ID,Checkout_ID,TransactionDate,SendAddress,EstCompletion")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Checkout_ID"] = new SelectList(_context.Checkouts, "Checkout_ID", "Checkout_ID", transaction.Checkout_ID);
+            ViewData["Payment_ID"] = new SelectList(_context.Payments, "Payment_ID", "Payment_ID", transaction.Payment_ID);
+            return View(transaction);
+        }
+
+        // GET: Transactions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Transactions == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+            ViewData["Checkout_ID"] = new SelectList(_context.Checkouts, "Checkout_ID", "Checkout_ID", transaction.Checkout_ID);
+            ViewData["Payment_ID"] = new SelectList(_context.Payments, "Payment_ID", "Payment_ID", transaction.Payment_ID);
+            return View(transaction);
+        }
+
+        // POST: Transactions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Transaction_ID,Payment_ID,Checkout_ID,TransactionDate,SendAddress,EstCompletion")] Transaction transaction)
+        {
+            if (id != transaction.Transaction_ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(transaction);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransactionExists(transaction.Transaction_ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Checkout_ID"] = new SelectList(_context.Checkouts, "Checkout_ID", "Checkout_ID", transaction.Checkout_ID);
+            ViewData["Payment_ID"] = new SelectList(_context.Payments, "Payment_ID", "Payment_ID", transaction.Payment_ID);
+            return View(transaction);
+        }
+
+        // GET: Transactions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Transactions == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions
+                .Include(t => t.Checkout)
+                .Include(t => t.Payment)
+                .FirstOrDefaultAsync(m => m.Transaction_ID == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(transaction);
+        }
+
+        // POST: Transactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transactions == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Transactions'  is null.");
+            }
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TransactionExists(int id)
+        {
+          return (_context.Transactions?.Any(e => e.Transaction_ID == id)).GetValueOrDefault();
+        }
+    }
+}
